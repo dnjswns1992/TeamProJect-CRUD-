68bd8bc9aa343e7f2fc9d211b85a0bc117a6aee7
Jaero0 <wjdwogh1224@gmail.com>
2024-02-08 10:37:25 +0900

    add
    프로젝트 추가
    gitignore application.properties 추가

---
+++ bgmbank/src/main/java/com/bgmbank/bgmbank/entity/User.java
@@ -0,0 +1,4 @@
+package com.bgmbank.bgmbank.entity;
+
+public class User {
+}
---
+++ bgmbank/src/test/java/com/bgmbank/bgmbank/BgmbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgmbank.bgmbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgmbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
