7e1d63f5d0cee0bb57cb173572e1d5b8a7165756
Jaero0 <wjdwogh1224@gmail.com>
2024-02-13 09:35:48 +0900

    fix
    sdf

--- bgmbank/src/main/java/com/bgmbank/bgmbank/constants/JwtConstants.java
+++ bgmbank/src/main/java/com/bgmbank/bgmbank/constants/JwtConstants.java
@@ -2,5 +2,5 @@ package com.bgmbank.bgmbank.constants;
 
 
 public class JwtConstants {
-    public static final String JWT_SECRET_KEY = "helsodkwhdhdsfkldsklha871984!@#!@#sdufsdf231@!12dahjsd";
+    public static final String JWT_SECRET_KEY = "helsodkwhdhds@#411fkldsklha871984!@#!@#sdufsdf231@!12dahjsd";
 }
